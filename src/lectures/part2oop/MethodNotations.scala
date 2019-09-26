10736bf3646f597c93598554b7e0554ef5c66973
Joao Micena (JP) <joao.micena@nubank.com.br>
2019-09-26 10:30:17 -0300

    video-2

---
+++ src/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,74 @@
+package lectures.part2oop
+import scala.language.postfixOps
+
+object MethodNotations extends App{
+
+  class Person(val name: String, favoriteMovie: String, val age: Int = 20) {
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(codename: String): Person = new Person(s"${this.name} $codename", this.favoriteMovie)
+    def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie. I'm $age years old"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+    def learns(course: String): String = s"$name learns $course"
+    def learnsScala: String = this.learns("Scala")
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // equivalent
+  // infix notation = operator notation
+
+  // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary + tom)
+  println(mary.+(tom))
+
+  println(1 + 2)
+  println(1.+(2)) // All operators are methods
+
+  // prefix notation
+  val x = -1
+  val y = 1.unary_- // equivalent
+  // unary_ prefix only works with - * ~ !
+  println(!mary)
+  println(mary.unary_!)
+
+  // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary()) // equivalent
+
+  /////
+  val maryCodenamed = mary + "the rockstar"
+  println(maryCodenamed())
+  println((mary + "the rockstar").apply())
+
+  val maryOlder = +mary
+  println(maryOlder())
+
+  println(mary learnsScala)
+
+  println(mary.apply(2))
+
+  /*
+    1. Overload the + operator
+       mary + "the rockstar" => new person "Mary (the rockstar)"
+
+    2. Add an age to the person class
+       Add an unary + operator => new person with the age + 1
+
+    3. Add a "learns" method in the Person class => "Mary learns Scala"
+       Add a learnsScala method, calls the learns method with "Scala"
+       use it in postfix notation
+
+    4. Overload the apply method
+       mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+}
--- src/lectures/part2oop/OOBasic.scala
+++ src/lectures/part2oop/OOBasic.scala
@@ -7,6 +7,20 @@ object OOBasic extends App {
   person.greet("Daniel")
   person.greet()
 
+  val imposter = new Writer("Franz", "Kafka", 1890)
+  val dosto = new Writer("Fiodor", "Dostoievski", 1910)
+  println(dosto.fullName())
+
+  val karamazov = new Novel("Karamazov Brothers", 1980, dosto)
+  println(karamazov.authorAge())
+  println(karamazov.isWrittenBy(dosto))
+  println(karamazov.isWrittenBy(imposter))
+
+  val counter = new Counter
+
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
 }
 
 // constructor
@@ -25,4 +39,56 @@ class Person(name: String, val age: Int = 0) {
 
 }
 
-// class parameters are NOT FIELDS. Use val to convert in fields
+/*
+Novel and a writer
+
+Writer: first name, surname, year
+  - method: full name
+
+Novel: name, year of release, author
+  - authorAge
+  - isWrittenBy(author)
+  - copy (new year of release) = new instance of Novel
+
+
+-----
+
+Counter class
+  - receives an int value
+  - method current count
+  - method increment/decrement => new counter
+  - overload inc/dec to receive an amount
+ */
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): String = s"$firstName $surname"
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+  def authorAge(): Int = year - author.year
+  def isWrittenBy(author: Writer): Boolean = author == this.author
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+}
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1)
+  } // immutability
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n - 1)
+  }
+
+  def print = println(count)
+}
